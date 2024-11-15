6af047651815822aba6efa78a13ae90f2ae5b5b5
petercoates <petercoates007@yahoo.com>
2024-11-15 03:04:57 +0000

    Tip calculator

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,6 +1,9 @@
 print("Welcome to the Tip Calculator!")
 bill = float(input("Kindly enter your bill : $"))
-tip_percentage = int(input("what percentage do you want to tip the waiter? (10 , 12 0r 15 ): "))
+tip = int(input("what percentage do you want to tip the waiter? (10 , 12 0r 15 ): "))
 no_of_guests = int(input("How many people are on this table? "))
-total_payment = round((bill + (bill * tip_percentage / 100) ) / no_of_guests, 2)
-print(f"You all will be required to pay: {total_payment}")
+
+tip_percentage = tip / 100
+total_tip = bill * tip_percentage
+total_bill = round((bill + total_tip) / no_of_guests, 2)
+print(f"You all will be required to pay: {total_bill} \nCiao!!!!")
