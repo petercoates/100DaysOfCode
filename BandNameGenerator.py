f9d9158b82b40a73ca5bb7193ca7915798d08567
petercoates <petercoates007@yahoo.com>
2024-11-15 01:17:18 +0000

    Added program to generate bandName

---
+++ BandNameGenerator.py
@@ -0,0 +1,5 @@
+name = input("Hey there! Please enter your name: \n")
+instruction = (" Welcome to the Band Name Generator!!! \nI will help you generate your band name")
+print("Hello " + name + instruction)
+pet = input("Whats the name of your pet (If you have any) : \n")
+print("Your band name can be: " + name + pet)
